c69cf47d3898b1c7b30977e71b02934f3e04ec74
manfred <manfred@b2217739-5cbd-4580-87d5-f689c6e5027b>
2010-04-06 09:50:06 +0000

    Make it possible to generate certificates signed by a CA with the Certificate class.
    git-svn-id: https://fngtps.com/svn/recorder/trunk/certificate_depot@51 b2217739-5cbd-4580-87d5-f689c6e5027b

--- lib/certificate_depot/certificate.rb
+++ lib/certificate_depot/certificate.rb
@@ -3,6 +3,11 @@ class CertificateDepot
     DEFAULT_VALIDITY_PERIOD = 3600 * 24 * 365 * 10
     X509v3 = 2
     
+    ATTRIBUTE_MAP = {
+      :organization  => 'O',
+      :email_address => 'emailAddress'
+    }
+    
     attr_accessor :certificate
     
     def initialize(certificate=nil)
@@ -14,15 +19,24 @@ class CertificateDepot
       to           = Time.now + DEFAULT_VALIDITY_PERIOD
       
       name         =  OpenSSL::X509::Name.new
-      name.add_entry('O', attributes[:organization])
+      ATTRIBUTE_MAP.each do |internal, x509_attribute|
+        name.add_entry(x509_attribute, attributes[internal]) if attributes[internal]
+      end
+      
+      if attributes[:ca_certificate]
+        issuer = attributes[:ca_certificate].subject
+      else
+        issuer = name
+      end
       
       @certificate = OpenSSL::X509::Certificate.new
       @certificate.subject    = name
-      @certificate.issuer     = name
+      @certificate.issuer     = issuer
       @certificate.not_before = from
       @certificate.not_after  = to
       @certificate.version    = X509v3
       @certificate.public_key = attributes[:public_key]
+      @certificate
     end
     
     def write_to(path)
@@ -38,14 +52,22 @@ class CertificateDepot
       @certificate.issuer
     end
     
-    def organization
-      self['O']
+    def subject
+      @certificate.subject
+    end
+    
+    def method_missing(method, *attributes, &block)
+      if x509_attribute = ATTRIBUTE_MAP[method.to_sym]
+        self[x509_attribute]
+      else
+        super
+      end
     end
     
     def [](key)
-      @certificate.issuer.to_a.each do |name, value, type|
+      @certificate.subject.to_a.each do |name, value, type|
         return value if name == key
-      end
+      end; nil
     end
     
     def self.generate(public_key)
--- test/unit/certificate_depot_certificate_test.rb
+++ test/unit/certificate_depot_certificate_test.rb
@@ -1,14 +1,31 @@
 require File.expand_path('../../test_helper', __FILE__)
 
 describe "CertificateDepot::Certificate" do
-  it "generates a new self-signed certificate" do
-    keypair = CertificateDepot::Keypair.generate
-    certificate = CertificateDepot::Certificate.generate(
+  before do
+    @ca_keypair     = CertificateDepot::Keypair.generate
+    @ca_certificate = CertificateDepot::Certificate.generate(
       :organization => 'Certificate Depot Test',
+      :public_key => @ca_keypair.public_key
+    )
+  end
+  
+  it "generates a new self-signed certificate" do
+    @ca_certificate.public_key.to_s.should == @ca_keypair.public_key.to_s
+    @ca_certificate['O'].should == 'Certificate Depot Test'
+    @ca_certificate.organization.should == 'Certificate Depot Test'
+  end
+  
+  it "generates a new client certificate" do
+    keypair     = CertificateDepot::Keypair.generate
+    certificate = CertificateDepot::Certificate.generate(
+      :ca_certificate => @ca_certificate,
+      :email_address => 'manfred@example.com',
       :public_key => keypair.public_key
     )
     certificate.public_key.to_s.should == keypair.public_key.to_s
-    certificate['O'].should == 'Certificate Depot Test'
-    certificate.organization.should == 'Certificate Depot Test'
+    certificate['emailAddress'].should == 'manfred@example.com'
+    certificate.email_address.should == 'manfred@example.com'
+    
+    certificate.issuer.to_s.should == @ca_certificate.subject.to_s
   end
 end
