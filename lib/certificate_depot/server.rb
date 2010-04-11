44d0779edf4e44e02e023d2f56f5bfcdbe2bf5b2
manfred <manfred@b2217739-5cbd-4580-87d5-f689c6e5027b>
2010-04-11 11:22:28 +0000

    Explain the more esoteric parts of the server architecture.
    git-svn-id: https://fngtps.com/svn/recorder/trunk/certificate_depot@87 b2217739-5cbd-4580-87d5-f689c6e5027b

--- lib/certificate_depot/server.rb
+++ lib/certificate_depot/server.rb
@@ -2,6 +2,23 @@ require 'socket'
 require 'fcntl'
 
 class CertificateDepot
+  # The CertificateDepot server is a pre-forking server. This basically means
+  # that it forks a pre-configured number of workers.
+  #
+  #   Server
+  #    |_ worker
+  #    |_ worker
+  #    |_ ...
+  #
+  # Workers hang around until something connects to the socket. The first
+  # worker to accept the request serves it. When workers die the server starts
+  # spawning new processes until it matches the configured process count.
+  # Workers are identified by their process ID or PID.
+  #
+  # The server creates a pipe between itself and each of the workers. We call
+  # these pipes lifelines. When a worker goes down the lifeline is severed.
+  # This is used as a signal by the server to spawn new workers. If the server
+  # goes down the workers use the same trick to notice this.
   class Server
     attr_accessor :socket, :depot
     
@@ -13,6 +30,8 @@ class CertificateDepot
       :max_connection_queue => 10
     }
     
+    # Create a new server instance. The first argument is a CertificateDepot
+    # instance. The second argument contains overrides to the DEFAULTS.
     def initialize(depot, options={})
       @depot = depot
       
@@ -22,38 +41,45 @@ class CertificateDepot
         @options[key] ||= DEFAULTS[key]
       end
       
-      # For Inter Process Communication we create a pair of pipes for each worker.
-      # The server process reads an writes from first IO object. The worker processes
-      # read and write from the second IO object.
+      # Contains the lifelines to all the workers. They are indexed by the
+      # worker's PID.
       @lifelines = {}
       
-      # Workers are instances of CertificateDepot::Worker. They are indexed by their
-      # own PID.
+      # Workers are instances of CertificateDepot::Worker. They are indexed by
+      # their own PID.
       @workers = {}
     end
     
-    # Start behaving like a server. This method blocks until the server needs to go down.
+    # Start behaving like a server. This method returns once the server has
+    # completely started.
     def listen
       trap_signals
       setup_socket
       run
     end
     
-    # Forks itself and starts the runloop for the server.
+    # Forks a process and starts a runloop in the fork. The runloop does
+    # worker housekeeping. It does so in three phases. First it removes all
+    # non-functional workers from its internal structures. After that it
+    # spawns new workers if it needs to. Finally it sleeps for a while so the
+    # the runloop doesn't keep busy all the time.
     def run
-      # fork do
+      fork do
         loop do
           reap_workers
           spawn_workers
           sleep
         end
-      # end
+      end
     end
     
-    # Figures out if any workers died and deletes them if they did
+    # Figures out if any workers died and deletes them from internal
+    # structures if they did.
     def reap_workers
       # Don't try to find more dead workers than the process count
       @workers.length.times do
+        # We use +waitpid+ to find any child process which has exited. It
+        # immediately returns when there aren't any dead processes.
         if pid = Process.waitpid(-1, Process::WNOHANG)
           despawn_worker(pid)
         else
@@ -68,15 +94,17 @@ class CertificateDepot
       @lifelines.delete(pid)
     end
     
-    # Figures out how many workers are currently running and creates new ones when needed.
+    # Figures out how many workers are currently running and creates new ones
+    # if needed.
     def spawn_workers
       missing_workers.times do
         worker = CertificateDepot::Worker.new(self)
         
         lifeline = IO.pipe
-        lifeline.each { |io| io.fcntl(Fcntl::F_SETFD, Fcntl::FD_CLOEXEC) }
         
         pid = fork do
+          # We close the server side of the pipe in this process otherwise we
+          # don't get a EOF when reading from it.
           lifeline.first.close
           worker.lifeline = lifeline.last
           worker.run
@@ -85,30 +113,39 @@ class CertificateDepot
         @workers[pid]   = worker
         @lifelines[pid] = lifeline.first
         
+        # We close the client side of the pipe in this process otherwise we
+        # don't get an EOF when reading from it.
         lifeline.last.close
       end
     end
     
-    # Sleeps until:
-    #   - Something in the application wants the main server loop to wake up OR
-    #   - 2 seconds go by
+    # Sleeps until someone wants the server main loop to wake up or when 2
+    # seconds go by. Workers can wake the server in two ways, either by
+    # writing anything to their lifeline or by severing the lifeline. The
+    # lifeline is severed when the worker dies.
     def sleep
+      # Returns with active IO objects if any of them are written to.
+      # Otherwise it times out after two seconds.
       needy = IO.select(@lifelines.values, nil, nil, 2)
-      needy.flatten.each do |pipe|
-        loop { pipe.read_nonblock(READ_BUFFER_SIZE) } unless pipe.closed?
+      # Read everything coming in on the lifelines and discard it because
+      # the contents doesn't matter.
+      needy.flatten.each do |lifeline|
+        loop { lifeline.read_nonblock(READ_BUFFER_SIZE) } unless pipe.closed?
       end if needy
     rescue EOFError, Errno::EAGAIN, Errno::EINTR
     end
     
     # Installs signal traps to listen for incoming signals to the process.
     def trap_signals
+      # Cleanup on exit
       trap(:EXIT) do
         @lifelines.each { |pid, lifeline| lifeline.close }
         socket.close
       end
     end
     
-    # Creates the socket the server listens on.
+    # Creates the socket the server listens on and binds it to the configured
+    # host and port.
     def setup_socket
       self.socket = Socket.new(Socket::AF_INET, Socket::SOCK_STREAM, 0)
       address = Socket.pack_sockaddr_in(@options[:port], @options[:host])
@@ -116,14 +153,14 @@ class CertificateDepot
       socket.listen(@options[:max_connection_queue])
     end
     
-    # Returns the number of workers that need to be created in order to get to the
-    # configured process count.
+    # Returns the number of workers that need to be created in order to get to
+    # the configured process count.
     def missing_workers
       @options[:process_count] - @workers.length
     end
     
-    # Creates a new server instance and starts listening on its configured hostname
-    # and port.
+    # Creates a new server instance and starts listening on its configured
+    # host and port. Returns once the server was started.
     def self.listen(depot, options={})
       server = new(depot, options)
       server.listen
